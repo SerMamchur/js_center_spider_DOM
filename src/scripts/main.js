d05ae6d3bb0295db7b3bbf5461eecbe9d77a90c3
Serhii Mamchur <kovaniysvit@gmail.com>
2025-03-17 11:35:36 +0100

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,5 @@
 const spider = document.querySelector('.spider');
 const parentEl = document.querySelector('.wall');
 
-parentEl.style.position = 'relative';
-spider.style.position = 'absolute';
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+spider.style.left = parentEl.scrollWidth / 2 - spider.offsetWidth / 2 + 'px';
+spider.style.top = parentEl.scrollHeight / 2 - spider.offsetHeight / 2 + 'px';
