27f274c19932f40b55e798e18bc06eb6ef2dfa31
Serhii Mamchur <kovaniysvit@gmail.com>
2025-03-17 11:37:24 +0100

    make build

---
+++ index.6e245ac5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.left=t.scrollWidth/2-e.offsetWidth/2+"px",e.style.top=t.scrollHeight/2-e.offsetHeight/2+"px";
+//# sourceMappingURL=index.6e245ac5.js.map
